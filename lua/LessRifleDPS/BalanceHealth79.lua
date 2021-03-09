f559ada620a2b56c6cc0cd616118c82eaf103e23
gthtdjlxbrueuk <80187438+gthtdjlxbrueuk@users.noreply.github.com>
2021-03-09 16:31:34 +0300

    Add files via upload

--- lua/LessRifleDPS/BalanceHealth79.lua
+++ lua/LessRifleDPS/BalanceHealth79.lua
@@ -2,3 +2,4 @@ kHiveHealth = 6000 --instead of 4000
 kHiveArmor = 750 --instead of 750
 kMatureHiveHealth = 8000 -- instead of 6000
 kMatureHiveArmor = 1400 
+kCommandStationHealth = 4000 -- instead of 3000
